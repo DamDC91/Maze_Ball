0c8a6a8cbbf570717ab250d55dcc98a1dcba0f2b
Damien <damiensporting@gmail.com>
2021-06-25 14:18:59 +0200

    remove physic_constant.h

--- include/physic_constant.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-
-const int EARTH_RADIUS = 6371; // km
-const double GRAVITATIONAL_CONSTANT = 6.67430 * 1e-11; // m³kg⁻¹s⁻²
-const double GRAVITY = 9.80665; // m/s²
-const double EARTH_MASS = 5.9722 * 1e24; // kg
